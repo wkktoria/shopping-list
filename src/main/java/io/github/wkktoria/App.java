01abc54ea03c4866f7ad6c906a1d6dfe795b03e2
wkktoria <127852624+wkktoria@users.noreply.github.com>
2023-11-16 21:04:49 +0100

    Create start-up project

---
+++ src/main/java/io/github/wkktoria/App.java
@@ -0,0 +1,12 @@
+package io.github.wkktoria;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        logger.info("App started");
+    }
+}
